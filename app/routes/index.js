1b9427876664f154050d4465c2093aa2526731ab
judaihyun1@gmail.com <judaihyun1@gmail.com>
2020-04-20 18:58:37 +0900

    fetch dummy data from firestore

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,18 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route{
-	@service store;
-	async model(){
-		return this.store.findAll('rental');
-	}
+	@service('firebase') fb;
 
-}
+	async model(){
+		const store = this.fb.store; 
+
+		let ret = await store.collection("TEST").doc('MYDOC')
+			.get()
+			.then(q=>{
+				console.log(q.data());
+			});
+		return ret;
+	}
+}	
+	
