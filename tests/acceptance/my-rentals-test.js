e88eb553a4ab80cc5374da520dc59c89ee53f1d5
judaihyun1@gmail.com <judaihyun1@gmail.com>
2020-04-22 18:42:32 +0900

    acceptance test update

--- tests/acceptance/my-rentals-test.js
+++ tests/acceptance/my-rentals-test.js
@@ -1,111 +1,103 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { module, test } from 'qunit';
 import { click, find, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | my rentals', function(hooks) {
-	
+module('Acceptance | my rentals', function (hooks) {
 	setupApplicationTest(hooks);
-  hooks.beforeEach(function(){
-	  this.owner.setupRouter();
-	this.owner.lookup('service:firebase');
-})
+	hooks.beforeEach(function () {
+		this.owner.setupRouter();
+		this.owner.lookup('service:firebase');
+	});
 
-	hooks.afterEach(function(){
-	this.owner.lookup('service:firebase').firebaseApp.delete();
-	this.owner.lookup('service:firebase').store.terminate();
+	hooks.afterEach(function () {
+		this.owner.lookup('service:firebase').firebaseApp.delete();
+		this.owner.lookup('service:firebase').store.terminate();
+	});
 
-	})
+	test('visiting /', async function (assert) {
+		await visit('/');
 
-test('visiting /', async function(assert) {
-	await visit('/');
-	
-	assert.equal(currentURL(), '/');
-	assert.dom('nav').exists();
-	assert.dom('h1').hasText('SuperRentals');
-	assert.dom('h2').hasText('Welcome to Super Rentals!');
-	
-	var h2Expect = 'Welcome to Super Rentals!';
-	assert.dom('h2').hasText(h2Expect);
-	
-	assert.dom('.jumbo a.button').hasText('About Us');
-	
-	await click('.jumbo a.button');
-	
-	assert.equal(currentURL(), '/about');
-  });
+		assert.equal(currentURL(), '/');
+		assert.dom('nav').exists();
+		assert.dom('h1').hasText('SuperRentals');
+		assert.dom('h2').hasText('Welcome to Super Rentals!');
 
-  test('viewing the details of a rental property', async function(assert){
-	await visit('/');
-	assert.dom('.rental').exists({count:3});
+		const h2Expect = 'Welcome to Super Rentals!';
+		assert.dom('h2').hasText(h2Expect);
 
-	await click('.rental:first-of-type a');
-	assert.equal(currentURL(), '/rentals/downtown-charm');
-  });
+		assert.dom('.jumbo a.button').hasText('About Us');
 
-  test('visiting /rentals/grand-old-mansion', async function(assert){
-	await visit('/rentals/grand-old-mansion');
+		await click('.jumbo a.button');
 
-	assert.equal(currentURL(), '/rentals/grand-old-mansion');
-	assert.dom('nav').exists();
-	assert.dom('h1').containsText('SuperRentals');
-	assert.dom('h2').containsText('Grand Old Mansion');
-	assert.dom('.rental.detailed').exists();
-	assert.dom('.share.button').hasText('Share on Twitter');
+		assert.equal(currentURL(), '/about');
+	});
 
-	let button = find('.share.button');
-	let tweetURL = new URL(button.href);
-    assert.equal(tweetURL.host, 'twitter.com');
+	test('viewing the details of a rental property', async function (assert) {
+		await visit('/');
+		assert.dom('.rental').exists({ count: 3 });
 
-    assert.equal(
-      tweetURL.searchParams.get('url'),
-      `${window.location.origin}/rentals/grand-old-mansion`
-    );
+		await click('.rental:first-of-type a');
+		assert.equal(currentURL(), '/rentals/downtown-charm');
+	});
 
-  });
-  
+	test('visiting /rentals/grand-old-mansion', async function (assert) {
+		await visit('/rentals/grand-old-mansion');
 
-  test('visiting /about', async function(assert){
+		assert.equal(currentURL(), '/rentals/grand-old-mansion');
+		assert.dom('nav').exists();
+		assert.dom('h1').containsText('SuperRentals');
+		assert.dom('h2').containsText('Grand Old Mansion');
+		assert.dom('.rental.detailed').exists();
+		assert.dom('.share.button').hasText('Share on Twitter');
 
-	await visit('/about');
-	assert.equal(currentURL(), '/about');
-	assert.dom('nav').exists();
-    assert.dom('h1').hasText('SuperRentals');
-	assert.dom('h2').hasText('About Super Rentals');
-	assert.dom('.jumbo a.button').hasText('Contact Us');
+		const button = find('.share.button');
+		const tweetURL = new URL(button.href);
+		assert.equal(tweetURL.host, 'twitter.com');
 
-	await click('.jumbo a.button');
-	assert.equal(currentURL(), '/getting-in-touch');
+		assert.equal(
+			tweetURL.searchParams.get('url'),
+			`${window.location.origin}/rentals/grand-old-mansion`,
+		);
+	});
 
-  });
 
-  test('visiting /getting-in-touch',async function(assert){
+	test('visiting /about', async function (assert) {
+		await visit('/about');
+		assert.equal(currentURL(), '/about');
+		assert.dom('nav').exists();
+		assert.dom('h1').hasText('SuperRentals');
+		assert.dom('h2').hasText('About Super Rentals');
+		assert.dom('.jumbo a.button').hasText('Contact Us');
 
-	await visit('/getting-in-touch');
-	assert.equal(currentURL(), '/getting-in-touch');
-	assert.dom('nav').exists();
-    assert.dom('h1').hasText('SuperRentals');
-	assert.dom('h2').hasText('Contact Us');
-	assert.dom('a.button').hasText('About');
+		await click('.jumbo a.button');
+		assert.equal(currentURL(), '/getting-in-touch');
+	});
 
-	await click('.jumbo a.button');
-	assert.equal(currentURL(), '/about');
+	test('visiting /getting-in-touch', async function (assert) {
+		await visit('/getting-in-touch');
+		assert.equal(currentURL(), '/getting-in-touch');
+		assert.dom('nav').exists();
+		assert.dom('h1').hasText('SuperRentals');
+		assert.dom('h2').hasText('Contact Us');
+		assert.dom('a.button').hasText('About');
 
-  });
+		await click('.jumbo a.button');
+		assert.equal(currentURL(), '/about');
+	});
 
-  test('navigating using the nav-bar', async (assert)=>{
+	test('navigating using the nav-bar', async (assert) => {
+		await visit('/');
+		assert.dom('nav').exists();
+		assert.dom('nav a.menu-index').hasText('SuperRentals');
 
-	await visit('/');
-	assert.dom('nav').exists();
-	assert.dom('nav a.menu-index').hasText('SuperRentals');
+		await click('nav a.menu-about');
+		assert.equal(currentURL(), '/about');
 
-	await click('nav a.menu-about');
-    assert.equal(currentURL(), '/about');
+		await click('nav a.menu-contact');
+		assert.equal(currentURL(), '/getting-in-touch');
 
-    await click('nav a.menu-contact');
-    assert.equal(currentURL(), '/getting-in-touch');
-
-    await click('nav a.menu-index');
-    assert.equal(currentURL(), '/');
-  });
-
+		await click('nav a.menu-index');
+		assert.equal(currentURL(), '/');
+	});
 });
